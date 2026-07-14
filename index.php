156dc2f00ffba283b93fdc4abd1b5ac0f8381622
gichukiwairua <gichukisimon@gmail.com>
2026-07-14 17:59:20 +0300

    Production hardening: disable error display, remove router.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+error_reporting(0);
+ini_set('display_errors', 0);
 
 require_once __DIR__ . '/config/database.php';
 require_once __DIR__ . '/config/schema.php';
